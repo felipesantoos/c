3b30a29be431494c738ecf905140cc727671d2f1
Felipe Santos <felipedssantos.dev@gmail.com>
2021-10-25 14:13:29 -0300

    Update 1006-media-1.c

--- 1006-media-1.c
+++ 1006-media-1.c
@@ -6,7 +6,7 @@ int main()
     scanf("%lf", &A);
     scanf("%lf", &B);
     MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
-    printf("MEDIA = %.5lf", MEDIA);
+    printf("MEDIA = %.5lf\n", MEDIA);
 
     return 0;
 }
